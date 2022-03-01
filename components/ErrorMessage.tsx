daa15be93eb0f7fabb204cf1e48aec21c0d2dd2f
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 02:37:02 +0300

    Remove unused components

--- components/ErrorMessage.tsx
+++
@@ -1,5 +0,0 @@
-const ErrorMessage = ({ errorMessage }: { errorMessage: string }) => {
-  return <div className="site-container">{errorMessage}</div>;
-};
-
-export default ErrorMessage;
--- components/Loading.tsx
+++
@@ -1,5 +0,0 @@
-const Loading = () => {
-  return <div className="site-container">Loading...</div>;
-};
-
-export default Loading;

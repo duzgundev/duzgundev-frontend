d1b5b1855ccbe80da72dcc2063652c695f02609d
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 02:54:50 +0300

    Make MyStack section responsive

--- components/MyStack.tsx
+++ components/MyStack.tsx
@@ -7,14 +7,14 @@ const MyStack = () => {
   return (
     <section className="w-full bg-[#F1F5F9] py-8">
       <div className="site-container">
-        <div className="site-section flex items-center justify-between">
-          <div>
+        <div className="site-section flex flex-col items-center justify-between text-center sm:flex-row sm:text-left">
+          <div className="mb-6 sm:mb-0">
             <h2 className="text-3xl font-semibold">My Stack</h2>
             <p className="mt-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
           </div>
-          <div className="flex items-center gap-9">
+          <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-9">
             <ReactIcon />
             <NextJSIcon />
             <TailwindIcon />

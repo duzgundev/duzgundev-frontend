857d451bf05a2cd6f17fa6b961488f9ad8a91296
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 04:42:41 +0300

    Add Images to blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import data from '../../contentrain/Blog.json';
+import dayjs from 'dayjs';
 
 const Blog: NextPage = () => {
   return (
@@ -12,20 +14,35 @@ const Blog: NextPage = () => {
       <div className="site-container space-y-6">
         <div className="site-section">
           {data.map((post) => {
-            const publishDate = new Date(post.createdAt).toDateString();
+            const publishDate = dayjs(post.createdAt).format('DD MMM YYYY');
 
             return (
-              <article className="space-y-2" key={post.ID}>
-                <h2 className="text-xl font-bold">
-                  <Link href={`/blog/${post.ID}`}>
-                    <a>{post.title}</a>
-                  </Link>
-                </h2>
-                <p>{post.excerpt}</p>
-                <div className="text-gray-400">
-                  <span>{publishDate}</span>
-                </div>
-              </article>
+              <Link href={`/blog/${post.ID}`} key={post.ID}>
+                <a>
+                  <article className="my-6 flex justify-between">
+                    <div className="basis-1/4">
+                      <Image
+                        src={require('../../public/contentrain/' +
+                          post.coverImage)}
+                        alt={post.title}
+                        width={421}
+                        height={237}
+                        objectFit="cover"
+                        layout="fixed"
+                      />
+                    </div>
+                    <div className="flex basis-3/5 flex-col">
+                      <h2 className="text-3xl font-bold">{post.title}</h2>
+                      <p className="mt-4">{post.excerpt}</p>
+                      <div className="mt-auto ml-auto mb-4 flex items-center gap-4 text-gray-400">
+                        <span>{post.author}</span>
+                        <div className="h-2 w-2 rounded-full bg-gray-400"></div>
+                        <span>{publishDate}</span>
+                      </div>
+                    </div>
+                  </article>
+                </a>
+              </Link>
             );
           })}
         </div>

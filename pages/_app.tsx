cd38de6e6bc81584977ea6ba95b5b7ba38dcb24b
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 03:08:51 +0300

    Adjust main margin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="text-gray-700 antialiased">
       <Header />
-      <main className="mb-20">
+      <main className="mb-8 sm:mb-20 md:mb-16">
         <Component {...pageProps} />
       </main>
       <Footer />

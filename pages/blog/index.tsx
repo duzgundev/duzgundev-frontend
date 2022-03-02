3a1e4ac985aa6dc42360ce1e5ed8b5b526f85f90
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 04:54:15 +0300

    Make blog page responsive

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -11,7 +11,7 @@ const Blog: NextPage = () => {
       <Head>
         <title>Mert Düzgün | Blog</title>
       </Head>
-      <div className="site-container space-y-6">
+      <div className="site-container">
         <div className="site-section">
           {data.map((post) => {
             const publishDate = dayjs(post.createdAt).format('DD MMM YYYY');
@@ -19,22 +19,20 @@ const Blog: NextPage = () => {
             return (
               <Link href={`/blog/${post.ID}`} key={post.ID}>
                 <a>
-                  <article className="my-6 flex justify-between">
-                    <div className="basis-1/4">
-                      <Image
-                        src={require('../../public/contentrain/' +
-                          post.coverImage)}
-                        alt={post.title}
-                        width={421}
-                        height={237}
-                        objectFit="cover"
-                        layout="fixed"
-                      />
-                    </div>
+                  <article className="my-6 flex flex-col items-center gap-4 md:flex-row md:items-stretch md:justify-between">
+                    <Image
+                      src={require('../../public/contentrain/' +
+                        post.coverImage)}
+                      alt={post.title}
+                      width={421}
+                      height={237}
+                      objectFit="cover"
+                      layout="intrinsic"
+                    />
                     <div className="flex basis-3/5 flex-col">
                       <h2 className="text-3xl font-bold">{post.title}</h2>
                       <p className="mt-4">{post.excerpt}</p>
-                      <div className="mt-auto ml-auto mb-4 flex items-center gap-4 text-gray-400">
+                      <div className="my-4 flex items-center gap-4 text-gray-400 md:mt-auto md:ml-auto">
                         <span>{post.author}</span>
                         <div className="h-2 w-2 rounded-full bg-gray-400"></div>
                         <span>{publishDate}</span>

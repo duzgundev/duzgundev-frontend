148e1b55d11c452787690f1b7d135b5a3c3df657
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-02 03:06:38 +0300

    Make articles section responsive

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -5,16 +5,16 @@ import data from '../contentrain/Blog.json';
 
 const Articles = () => {
   return (
-    <section className="site-container mt-24">
-      <h2 className="col-start-4 col-end-10 text-center text-4xl font-bold">
+    <section className="site-container mt-12 sm:mt-24">
+      <h2 className="col-span-full text-center text-2xl font-bold sm:text-4xl lg:col-start-4 lg:col-end-10">
         My thougts and experiences about development ecosystem
       </h2>
-      <div className="col-span-full mt-16 grid grid-cols-3 gap-4">
+      <div className="col-span-full mt-8 grid grid-cols-1 gap-10 sm:mt-16 sm:grid-cols-2 sm:gap-4 md:grid-cols-3">
         {data.map((post) => {
           const publishDate = dayjs(post.createdAt).format('DD MMM YYYY');
 
           return (
-            <article className="space-y-2" key={post.ID}>
+            <article className="space-y-1 sm:space-y-2" key={post.ID}>
               <Link href={`/blog/${post.ID}`}>
                 <a>
                   {post.coverImage && (
@@ -24,14 +24,17 @@ const Articles = () => {
                       width={421}
                       height={237}
                       objectFit="cover"
+                      layout="responsive"
                     />
                   )}
-                  <div className="mt-6 flex items-center gap-4 text-gray-400">
+                  <div className="mt-3 flex items-center gap-4 text-gray-400 sm:mt-6">
                     <span>{post.author}</span>
                     <div className="h-2 w-2 rounded-full bg-gray-400"></div>
                     <span>{publishDate}</span>
                   </div>
-                  <h2 className="mt-3 text-xl font-bold">{post.title}</h2>
+                  <h2 className="mt-2 text-xl font-bold sm:mt-3">
+                    {post.title}
+                  </h2>
                 </a>
               </Link>
             </article>

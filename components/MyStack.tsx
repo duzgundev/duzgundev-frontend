63a19a4d4ae5bfa83899b4351b684c191eaefaa2
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-20 23:48:31 +0300

    Remove unncessary Link usage

--- components/MyStack.tsx
+++ components/MyStack.tsx
@@ -2,7 +2,6 @@ import ReactIcon from '../public/react.svg';
 import NextJSIcon from '../public/nextjs.svg';
 import TailwindIcon from '../public/tailwindcss.svg';
 import ReduxIcon from '../public/redux.svg';
-import Link from 'next/link';
 
 const MyStack = () => {
   return (
@@ -16,26 +15,18 @@ const MyStack = () => {
             </p>
           </div>
           <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-9">
-            <Link href="https://reactjs.org/">
-              <a target="_blank">
-                <ReactIcon />
-              </a>
-            </Link>
-            <Link href="https://nextjs.org/">
-              <a target="_blank">
-                <NextJSIcon />
-              </a>
-            </Link>
-            <Link href="https://tailwindcss.com/">
-              <a target="_blank">
-                <TailwindIcon />
-              </a>
-            </Link>
-            <Link href="https://redux.js.org/">
-              <a target="_blank">
-                <ReduxIcon />
-              </a>
-            </Link>
+            <a href="https://reactjs.org/" rel="noreferrer" target="_blank">
+              <ReactIcon />
+            </a>
+            <a href="https://nextjs.org/" rel="noreferrer" target="_blank">
+              <NextJSIcon />
+            </a>
+            <a href="https://tailwindcss.com/" rel="noreferrer" target="_blank">
+              <TailwindIcon />
+            </a>
+            <a href="https://redux.js.org/" rel="noreferrer" target="_blank">
+              <ReduxIcon />
+            </a>
           </div>
         </div>
       </div>

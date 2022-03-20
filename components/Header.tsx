adfe7f007f7571d9e9bdf089a72ece166b0fbaa7
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-20 23:36:22 +0300

    Add logo to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import MdLogo from '../public/md-logo.svg';
 
 const Header = () => {
   const router = useRouter();
@@ -32,10 +33,14 @@ const Header = () => {
       )}
     >
       <div className="site-container">
-        <div className="site-section flex justify-center py-5 sm:justify-between">
+        <div className="site-section flex justify-between py-5">
           <Link href="/">
-            <a className="hidden text-2xl font-bold sm:block">Mert Düzgün</a>
+            <a className="flex items-center text-2xl font-bold">
+              <MdLogo />
+              <span className="ml-2 hidden sm:block">Mert Düzgün</span>
+            </a>
           </Link>
+
           <nav className="flex items-center">
             <ul className="flex space-x-4">
               <li>

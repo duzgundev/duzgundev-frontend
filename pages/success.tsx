2c98e57f40ca392e30df570305711bbb4765fbfc
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-20 23:22:10 +0300

    Refactor success page

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+
 const Success = () => {
-  return <div>Form successfully submitted!</div>;
+  return (
+    <section className="site-container">
+      <div className="site-section flex h-[60vh] flex-col items-center justify-center text-center">
+        <h1 className="text-4xl font-bold">Form successfully submitted!</h1>
+        <Link href="/">
+          <a className="mt-4 text-xl font-semibold underline">
+            Go back to home page
+          </a>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Success;

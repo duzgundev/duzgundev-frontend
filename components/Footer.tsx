d8ccc03fdc425612a162fe26215dce7cb10152fd
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-20 23:50:32 +0300

    Remove unncessary Link usage

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,6 @@ import LinkedInIcon from '../public/linkedin.svg';
 import GithubIcon from '../public/github.svg';
 import CopyrightIcon from '../public/copyright.svg';
 import InstagramIcon from '../public/instagram.svg';
-import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -16,26 +15,34 @@ const Footer = () => {
             <p>2022</p>
           </div>
           <div className="flex gap-2 sm:gap-4">
-            <Link href="https://github.com/duzgundev">
-              <a target="_blank">
-                <GithubIcon />
-              </a>
-            </Link>
-            <Link href="https://twitter.com/duzgundev">
-              <a target="_blank">
-                <TwitterIcon />
-              </a>
-            </Link>
-            <Link href="https://linkedin.com/in/mertduzgun">
-              <a target="_blank">
-                <LinkedInIcon />
-              </a>
-            </Link>
-            <Link href="https://instagram.com/duzgundev">
-              <a target="_blank">
-                <InstagramIcon />
-              </a>
-            </Link>
+            <a
+              target="_blank"
+              href="https://github.com/duzgundev"
+              rel="noreferrer"
+            >
+              <GithubIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/duzgundev"
+              rel="noreferrer"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://linkedin.com/in/mertduzgun"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              target="_blank"
+              href="https://instagram.com/duzgundev"
+              rel="noreferrer"
+            >
+              <InstagramIcon />
+            </a>
           </div>
         </div>
       </div>

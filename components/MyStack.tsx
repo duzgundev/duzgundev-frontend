783033ca65b5f7480fb1867ea1c19211cf29973f
Mert Duzgun <duzgunnmert@gmail.com>
2022-03-20 23:43:05 +0300

    Refactor MyStack section

--- components/MyStack.tsx
+++ components/MyStack.tsx
@@ -2,6 +2,7 @@ import ReactIcon from '../public/react.svg';
 import NextJSIcon from '../public/nextjs.svg';
 import TailwindIcon from '../public/tailwindcss.svg';
 import ReduxIcon from '../public/redux.svg';
+import Link from 'next/link';
 
 const MyStack = () => {
   return (
@@ -11,14 +12,30 @@ const MyStack = () => {
           <div className="mb-6 sm:mb-0">
             <h2 className="text-3xl font-semibold">My Stack</h2>
             <p className="mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              I mostly use these technologies to build awesome stuff.
             </p>
           </div>
           <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-9">
-            <ReactIcon />
-            <NextJSIcon />
-            <TailwindIcon />
-            <ReduxIcon />
+            <Link href="https://reactjs.org/">
+              <a target="_blank">
+                <ReactIcon />
+              </a>
+            </Link>
+            <Link href="https://nextjs.org/">
+              <a target="_blank">
+                <NextJSIcon />
+              </a>
+            </Link>
+            <Link href="https://tailwindcss.com/">
+              <a target="_blank">
+                <TailwindIcon />
+              </a>
+            </Link>
+            <Link href="https://redux.js.org/">
+              <a target="_blank">
+                <ReduxIcon />
+              </a>
+            </Link>
           </div>
         </div>
       </div>
